1b87c233c5170bf4baf5009b70bd106e61a30daa
RasWeb <rasmuspalm@fastmail.se>
2021-11-29 15:08:14 +0100

    fixed a better javaScript modal

---
+++ dist/index.2aa4842d.js
@@ -0,0 +1,23 @@
+var modalBtns = [
+    ...document.querySelectorAll(".button")
+];
+modalBtns.forEach(function(btn) {
+    btn.onclick = function() {
+        var modal = btn.getAttribute("data-modal");
+        document.getElementById(modal).style.display = "block";
+    };
+});
+var closeBtns = [
+    ...document.querySelectorAll(".close")
+];
+closeBtns.forEach(function(btn) {
+    btn.onclick = function() {
+        var modal = btn.closest(".modal");
+        modal.style.display = "none";
+    };
+});
+window.onclick = function(event) {
+    if (event.target.className === "modal") event.target.style.display = "none";
+};
+
+//# sourceMappingURL=index.2aa4842d.js.map
